268d709e61e88a0cc108b2a1b22cfc18c633ad5d
yangej <yangej0218@gmail.com>
2022-01-30 00:46:02 +0800

    fix(otto/lang-picker): correct lang picker styles

--- src/common/Button/CustomButton.tsx
+++ src/common/Button/CustomButton.tsx
@@ -1,17 +1,40 @@
-import { Box } from '@material-ui/core'
-import React, { ComponentProps, CSSProperties, forwardRef } from 'react'
+import { ElementType, ComponentProps, forwardRef, CSSProperties } from 'react';
 
-type BoxProps = ComponentProps<typeof Box>
-type ButtonType = 'outline' | 'solid' | 'icon'
+import { Box, SvgIcon, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '44px',
+    borderRadius: '10px',
+    padding: '0 30px',
+    cursor: 'pointer',
+    fontWeight: 600,
+    fontSize: '16px',
+    textDecoration: 'none',
+    [theme.breakpoints.down('sm')]: {
+      padding: '10px 20px'
+    },
+  },
+  icon: {
+    padding: '10px',
+    borderRadius: '10px'
+  }
+}));
+
+type BoxProps = ComponentProps<typeof Box>;
+type ButtonType = 'outline' | 'solid' | 'icon';
 type Props = BoxProps & {
-  text?: string
-  type?: ButtonType
-  Icon?: any
-  href?: string
-  target?: string
-}
+  text?: string;
+  type?: ButtonType;
+  Icon?: ElementType<any>;
+  href?: string;
+  target?: string;
+};
 
-type ButtonConfig = Partial<Pick<BoxProps, 'bgcolor' | 'color' | 'border'>>
+type ButtonConfig = Partial<Pick<BoxProps, 'bgcolor' | 'color' | 'border'>>;
 const STYLE: Record<ButtonType, ButtonConfig> = {
   outline: {
     border: '2px solid',
@@ -27,41 +50,40 @@ const STYLE: Record<ButtonType, ButtonConfig> = {
     bgcolor: '',
     color: 'text.primary',
   },
-}
+};
+const ICON_BASE_STYLE: CSSProperties = { width: '20px', height: '20px', verticalAlign: 'middle' };
+const ICON_SPACING: CSSProperties = { marginRight: '10px', marginLeft: '-10px' };
 
 const CustomButton = forwardRef<any, Props>(
   ({ type = 'solid', className = '', component, text, Icon, ...props }, ref) => {
-    component = props.href ? 'a' : component
+    component = props.href ? 'a' : component;
+    const classes = useStyles();
     if (type === 'icon') {
       return (
         <Box
           {...({ ref } as any)}
-          className={'icon-button ' + className}
+          className={`${classes.icon} ${className}`}
           {...STYLE['icon']}
           component={component}
           {...props}
         >
-          {Icon && <Icon />}
+          {Icon && <SvgIcon component={Icon} style={{ ...ICON_BASE_STYLE }} />}
         </Box>
-      )
+      );
     }
     return (
       <Box
         {...({ ref } as any)}
-        className={'custom-button ' + className}
+        className={`${classes.button} ${className}`}
         {...STYLE[type]}
         component={component}
         {...props}
       >
-        {Icon && (
-          <div style={{ marginRight: '10px', marginLeft: '-10px' }}>
-            <Icon />
-          </div>
-        )}
+        {Icon && <SvgIcon component={Icon} style={{ ...ICON_BASE_STYLE, ...ICON_SPACING }} />}
         <p>{text}</p>
       </Box>
-    )
-  }
-)
+    );
+  },
+);
 
-export default CustomButton
+export default CustomButton;
