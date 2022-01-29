235c9e8b5cb64f9efba85293cb3ad17103bb1005
yangej <yangej0218@gmail.com>
2022-01-30 00:06:40 +0800

    fix(otto/countdown): correct countdown section styles

--- src/Otto/OttoCountdownSection/index.tsx
+++ src/Otto/OttoCountdownSection/index.tsx
@@ -1,30 +1,69 @@
+import { makeStyles } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 import OttoBuyClamHint from './OttoBuyClamHint'
 import OttoDecoLeftImage from './image-countdown_deco_left.png'
 import OttoDecoRightImage from './image-countdown_deco_right.png'
 
-import { makeStyles } from '@material-ui/core'
 import Image from 'next/image'
 
 const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: theme.palette.mode.darkBlue,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '60px 245px 60px 245px',
+    overflow: 'hidden',
+    position: 'relative',
+    zIndex: 0,
+    [theme.breakpoints.down('md')]: {
+      padding: '80px 20px 80px 20px',
+    }
   },
-  h4: {
+  decoLeft: {
+    position: 'absolute',
+    width: '30%',
+    zIndex: -1,
+    top: 0,
+    left: 0,
+    [theme.breakpoints.down('md')]: {
+      width: '40%',
+    }
+  },
+  decoRight: {
+    position: 'absolute',
+    width: '30%',
+    zIndex: -1,
+    bottom: '-2px',
+    right: 0,
+    [theme.breakpoints.down('md')]: {
+      width: '40%',
+    }
+  },
+  title: {
     color: theme.palette.mode.white,
+    fontSize: '48px',
+    fontWeight: 800,
+    textAlign: 'center',
+    margin: 0,
+    marginBottom: '20px',
   },
+  content: {
+    color: theme.palette.otter.white,
+    fontSize: '28px'
+  }
 }))
 
 const OttoCountdownSection = () => {
   const classes = useStyles()
   const { t } = useTranslation()
   return (
-    <div className={`otto-countdown__container ${classes.container}`}>
-      <div className="otto-countdown__deco left">
+    <div className={classes.container}>
+      <div className={classes.decoLeft}>
         <Image src={OttoDecoLeftImage} alt="countdown left" />
       </div>
-      <h4 className={`otto-countdown__title ${classes.h4}`}>{t('otto.countdown.mintTitle')}</h4>
-      <p className="otto-countdown__content">COMING SOON</p>
+      <h4 className={classes.title}>{t('otto.countdown.mintTitle')}</h4>
+      <p className={classes.content}>COMING SOON</p>
       {/* <Countdown
         title={t('otto.countdown.mintSubtitle')}
         color="mode.white"
@@ -34,7 +73,7 @@ const OttoCountdownSection = () => {
       /> */}
       {/* TODO|OTTO: add time up callback */}
       <OttoBuyClamHint />
-      <div className="otto-countdown__deco right">
+      <div className={classes.decoRight}>
         <Image src={OttoDecoRightImage} alt="countdown right" />
       </div>
     </div>
