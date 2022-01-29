96c49af241f5359bf7552cd5d55e1c3c2db42b92
yangej <yangej0218@gmail.com>
2022-01-30 00:58:56 +0800

    fix(otto/header): correct header styles

--- src/Otto/OttoHeader/OttoDesktopHeader.tsx
+++ src/Otto/OttoHeader/OttoDesktopHeader.tsx
@@ -1,43 +1,91 @@
-import { Link, makeStyles } from '@material-ui/core'
-import { LandingPageLink } from 'src/constants'
-import { LinkMetadata } from './type'
-import NewChip from 'src/common/NewChip'
-import LanguagePicker from 'src/common/LanguagePicker'
-import Logo from './Logo'
-import { light } from 'src/themes'
+import { Link, makeStyles } from '@material-ui/core';
+import { LandingPageLink } from 'src/constants';
+import { LinkMetadata } from './type';
+import NewChip from 'src/common/NewChip';
+import LanguagePicker from 'src/common/LanguagePicker';
+import Logo from './Logo';
+import { light } from 'src/themes';
 
 const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: light.palette.mode.white,
     borderBottom: `1px solid ${light.palette.mode.lightGray400}`,
+    position: 'fixed',
+    zIndex: 2,
+    top: 0,
+    left: 0,
+    right: 0,
+    padding: '10px 0',
   },
-}))
+  container: {
+    width: '100%',
+    padding: '0 245px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+  },
+  sectionLeft: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  sectionRight: {
+    flex: 1,
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  link: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    marginLeft: '40px',
+    whiteSpace: 'nowrap',
+    fontSize: '16px',
+    fontWeight: 700,
+    lineHeight: '24px',
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+  },
+  langPicker: {
+    marginLeft: '20px',
+    color: theme.palette.mode.otterDark
+  }
+}));
 
 interface Props {
-  linkMetadata: LinkMetadata[]
+  linkMetadata: LinkMetadata[];
 }
 
 export default function OttoDesktopHeader({ linkMetadata }: Props) {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
-    <header className={`otto-header-desktop ${classes.header}`}>
-      <div className="otto-header-desktop__container">
-        <div className="otto-header-desktop__section left">
-          <a href={LandingPageLink} className="otto-header-logo">
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <div className={classes.sectionLeft}>
+          <a href={LandingPageLink} className={classes.logo}>
             <Logo />
           </a>
         </div>
-        <div className="otto-header-desktop__section right">
+        <div className={classes.sectionRight}>
           {linkMetadata.map((metadata, i) => (
-            <Link key={i} className="otto-header-desktop__link" href={metadata.href} target="__blank">
+            <a
+              key={i}
+              className={classes.link}
+              href={metadata.href}
+              target="__blank"
+            >
               {metadata.text} {metadata.new && <NewChip marginLeft="10px" />}
-            </Link>
+            </a>
           ))}
-          <div id="lang-picker">
+          <div className={classes.langPicker}>
             <LanguagePicker border={false} />
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
