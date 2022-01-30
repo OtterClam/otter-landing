33f178ec92184de0f1a920a99472f18253746b89
yangej <yangej0218@gmail.com>
2022-01-30 16:44:39 +0800

    feat(footer): add footer deco

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -2,20 +2,35 @@ import { Link, makeStyles } from '@material-ui/core';
 import { RESOURCES_LINKS, INVOLVED_LINKS } from './constant';
 import PartnershipLightModeImage from 'public/logo-partnership-light.png';
 import SlowmistLightModeImage from 'public/logo-slowmist-light.png';
+import FooterDeco from './images/deco-footer.png';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const useStyles = makeStyles((theme) => ({
+  deco: {
+    width: '618px',
+    height: '240px',
+    position: 'relative',
+    margin: '0 auto -96px auto',
+    zIndex: 1,
+    [theme.breakpoints.down('md')]: {
+      width: '375px',
+      height: '145px',
+      margin: '0 auto -56px auto',
+    },
+  },
   container: {
+    position: 'relative',
     backgroundColor: theme.palette.mode.lightGray200,
     display: 'grid',
     gridTemplateColumns: '2fr 1fr 1fr',
     gridColumnGap: '30px',
-    padding: '94px 245px',
+    padding: '156px 245px 94px 245px',
     [theme.breakpoints.down('md')]: {
       gridTemplateColumns: 'auto',
       gridColumnGap: '0px',
       gridRowGap: '40px',
-      padding: '40px 20px',
+      padding: '80px 20px 40px 20px',
     },
   },
   column: {
@@ -88,81 +103,82 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const OttoFooter = () => {
+const Footer = () => {
   const classes = useStyles();
+  const { pathname } = useRouter();
+  const showDeco = pathname !== '/otto';
   return (
-    <div className={`${classes.container}`}>
-      <div className={classes.column}>
-        <h5 className={classes.h5}>
-          IN PARTNERSHIP WITH
-        </h5>
-        <div className={`${classes.column} ${classes.partnership}`}>
-          <div className={classes.partnershipLogo}>
-            <Image
-              src={PartnershipLightModeImage}
-              width={310}
-              height={62}
-              alt="partnership"
-              layout="fixed"
-            />
-          </div>
+    <>
+      {showDeco && (
+        <div className={classes.deco}>
+          <Image layout="fill" src={FooterDeco.src} alt="footer deco" />
+        </div>
+      )}
+      <div className={`${classes.container}`}>
+        <div className={classes.column}>
+          <h5 className={classes.h5}>IN PARTNERSHIP WITH</h5>
+          <div className={`${classes.column} ${classes.partnership}`}>
+            <div className={classes.partnershipLogo}>
+              <Image
+                src={PartnershipLightModeImage}
+                width={310}
+                height={62}
+                alt="partnership"
+                layout="fixed"
+              />
+            </div>
 
-          <div className={classes.partnershipText}>
-            <p className={classes.body2}>
-              © 2022 OtterClam All Rights Reserved
-            </p>
-            <p
-              className={`${classes.body2} ${classes.auditText}`}
-            >
-              Audited by
-              <div className={classes.slowmist}>
-                <Image
-                  src={SlowmistLightModeImage}
-                  width={80}
-                  height={18}
-                  alt="slowmist"
-                  layout="fixed"
-                />
-              </div>
-            </p>
+            <div className={classes.partnershipText}>
+              <p className={classes.body2}>
+                © 2022 OtterClam All Rights Reserved
+              </p>
+              <p className={`${classes.body2} ${classes.auditText}`}>
+                Audited by
+                <div className={classes.slowmist}>
+                  <Image
+                    src={SlowmistLightModeImage}
+                    width={80}
+                    height={18}
+                    alt="slowmist"
+                    layout="fixed"
+                  />
+                </div>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.column}>
+          <h5 className={classes.h5}>RESOURCES</h5>
+          <div className={classes.links}>
+            {RESOURCES_LINKS.map((link, index) => (
+              <Link
+                key={`resource-${index}`}
+                className={classes.link}
+                href={link.href}
+                target="__blank"
+              >
+                {link.text}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className={classes.column}>
+          <h5 className={classes.h5}>GET INVOLVED</h5>
+          <div className={classes.links}>
+            {INVOLVED_LINKS.map((link, index) => (
+              <Link
+                key={`involved-${index}`}
+                className={classes.link}
+                href={link.href}
+                target="__blank"
+              >
+                {link.text}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
-      <div className={classes.column}>
-        <h5 className={classes.h5}>
-          RESOURCES
-        </h5>
-        <div className={classes.links}>
-          {RESOURCES_LINKS.map((link, index) => (
-            <Link
-              key={`resource-${index}`}
-              className={classes.link}
-              href={link.href}
-              target="__blank"
-            >
-              {link.text}
-            </Link>
-          ))}
-        </div>
-      </div>
-      <div className={classes.column}>
-        <h5 className={classes.h5}>
-          GET INVOLVED
-        </h5>
-        <div className={classes.links}>
-          {INVOLVED_LINKS.map((link, index) => (
-            <Link
-              key={`involved-${index}`}
-              className={classes.link}
-              href={link.href}
-              target="__blank"
-            >
-              {link.text}
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
-export default OttoFooter;
+export default Footer;
