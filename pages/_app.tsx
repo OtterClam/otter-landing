55a59567fbce9128a76c81fe754b76d7593dfb9d
yangej <yangej0218@gmail.com>
2022-02-16 23:22:34 +0800

    feat(loading): apply loading screen component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,30 @@
-import { ThemeProvider as MUIThemeProvider } from '@material-ui/core'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import TagManager from 'react-gtm-module'
-import Footer from 'src/common/Footer'
-import Header from 'src/common/Header'
-import 'src/i18n'
-import { light } from 'src/themes'
-import { GlobalStyle, theme } from 'src/themes/styled-components'
-import { ThemeProvider as StyledThemeProvider } from 'styled-components'
-import '../styles/fonts.scss'
+import { ThemeProvider as MUIThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import TagManager from 'react-gtm-module';
+import Footer from 'src/common/Footer';
+import Header from 'src/common/Header';
+import LoadingScreen from 'src/common/LoadingScreen';
+import 'src/i18n';
+import { light } from 'src/themes';
+import { GlobalStyle, theme } from 'src/themes/styled-components';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import '../styles/fonts.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-PVKBZHG' })
-  }, [])
+    TagManager.initialize({ gtmId: 'GTM-PVKBZHG' });
+  }, []);
+
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadCallback = () => setLoading(false);
+    window.addEventListener('load', loadCallback);
+    return () => {
+      window.removeEventListener('load', loadCallback);
+    };
+  }, [setLoading]);
   return (
     <MUIThemeProvider theme={light}>
       <StyledThemeProvider theme={theme}>
@@ -23,9 +33,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Header />
         <Component {...pageProps} />
         <Footer />
+        <LoadingScreen show={loading} />
       </StyledThemeProvider>
     </MUIThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
